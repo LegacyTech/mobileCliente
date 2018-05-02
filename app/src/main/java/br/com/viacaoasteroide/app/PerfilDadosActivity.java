bceecb2442f17a705fc761191c73e36bec64cad7
MATHEUS ALVES DE SOUZA <16255204@CFP127ALUNO.LOCAL>
2018-05-02 11:17:10 -0300

    Commit PerfilDados

--- app/src/main/java/br/com/viacaoasteroide/app/PerfilDadosActivity.java
+++ app/src/main/java/br/com/viacaoasteroide/app/PerfilDadosActivity.java
@@ -5,8 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 16255204 on 04/04/2018.
@@ -14,7 +19,11 @@ import android.widget.CalendarView;
 
 public class PerfilDadosActivity extends AppCompatActivity {
 
+    Spinner dia, mes, ano;
     Button btn_cad_usuario; //Botão de salvar
+    ArrayList<String> array_dias = new ArrayList<>();
+    ArrayList<String> array_meses = new ArrayList<>();
+    ArrayList<String> array_anos = new ArrayList<>();
     boolean novo;
 
 
@@ -24,6 +33,35 @@ public class PerfilDadosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.perfildados_activity);
 
+        ArrayAdapter<String> adapterDia, adapterMes, adapterAno;
+
+        //Parte da data
+
+        for( int i = 1 ; i <= 30; i++ ){
+            array_dias.add("" + i);
+        }
+
+        for( int i = 1 ; i <= 12; i++ ){
+            array_meses.add("" + i);
+        }
+
+        for( int i = 2018 ; i >= 1920; i-- ){
+            array_anos.add("" + i);
+        }
+
+        dia = findViewById(R.id.dia_perfildados);
+        mes = findViewById(R.id.mes_perfildados);
+        ano = findViewById(R.id.ano_perfildados);
+
+        adapterDia = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, array_dias);
+        adapterMes = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, array_meses);
+        adapterAno = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, array_anos);
+
+        dia.setAdapter(adapterDia);
+        mes.setAdapter(adapterMes);
+        ano.setAdapter(adapterAno);
+
+        //Verificação
         btn_cad_usuario = findViewById(R.id.btn_cad_usuario);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Mostrar o botão de voltar
@@ -36,7 +74,6 @@ public class PerfilDadosActivity extends AppCompatActivity {
             btn_cad_usuario.setText("Cadastrar");
 
         }
-
 
     }
 
